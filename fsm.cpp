5b170dfe1f4310d7a1e8b0672d17138958b20376
jaderobertsg2019 <jaderobertsg2019@gmail.com>
2025-05-25 13:37:39 -0500

    split fsm.cpp main() into more digestible chunks

--- fsm.cpp
+++ fsm.cpp
@@ -10,158 +10,186 @@
 #include "nfaParser.h"
 using namespace std;
 
+void dfaParsing(DfaParser dfaparser)
+{
+	dfaparser.clearWhiteSpace(); // remove spaces and tabs
+	dfaparser.parse_dfa();
+	/*
+	 * parse dfa according to the CFG G (V, SIGMA, R, S)
+	 * where V = {dfa, q, sigma, delta, start_state, f, list, transitions, transition, primary},
+	 *		SIGMA = {Q, SIGMA, DELTA, START_STATE, F, EQUAL, LBRACE, RBRACE, SEMICOLON, LPAREN, RPAREN, ID, NUM},
+	 *		R = {
+	 *
+	 *		dfa			-> q sigma delta start_state f,
+	 *		q			-> Q EQUAL LBRACE list LBRACE SEMICOLON,
+	 *		sigma		-> SIGMA EQUAL LBRACE list RBRACE SEMICOLON,
+	 *		delta		-> DELTA EQUAL LBRACE transitions RBRACE SEMICOLON,
+	 *		start_state	-> START_STATE EQUAL primary SEMICOLON,
+	 *		f			-> F EQUAL LBRACE list RBRACE SEMICOLON,
+	 *		list		-> primary COMMA list || primary,
+	 *		transitions	-> transiton COMMA transitions || transition,
+	 *		transition	-> DELTA LPAREN primary COMMA primary RPAREN EQUALS primary,
+	 *		primary		-> ID || NUM}
+	 *
+	 *		S = dfa
+	 */
+}
+
+void dfaComputing(Menus menus, DfaParser dfaparser)
+{
+	string userInDFA = "";
+	while (userInDFA != "q")
+	{
+		menus.setMenu(Menus::DFA_OPS);
+		menus.printCurrentMenu();
+		cin >> userInDFA;
+		userInDFA = tolower(userInDFA[0]);
+
+		string w;
+		if (userInDFA == "w")
+		{
+			menus.setMenu(Menus::W_IN);
+			menus.printCurrentMenu();
+			DfaComputer computer;
+			cin >> w;
+			if (computer.startExecution(w, dfaparser))
+				cout << "M accepts w" << endl;
+			else
+				cout << "M does not accept w" << endl;
+		}
+		else if (userInDFA == "q")
+		{
+			cout << "done with this dfa." << endl;
+		}
+		else
+		{
+			cout << "invalid input, try again" << endl;
+		}
+	}
+}
+
+void dfaHandling(Menus menus)
+{
+	string txt;
+	menus.setMenu(Menus::INPUT_DFA);
+	menus.printCurrentMenu();
+	cin >> txt;
+	// read dfa.txt
+	fstream newfile;
+	newfile.open(txt, ios::in);
+	if (newfile.is_open())
+	{
+		DfaParser dfaparser;
+		dfaparser.dfaDef = "";
+		// concatenate each line of dfa.txt to dfaDef, ignoring all newlines in text file
+		string line;
+		while (getline(newfile, line))
+		{
+			dfaparser.dfaDef += line;
+		}
+		dfaParsing(dfaparser);
+		dfaComputing(menus, dfaparser);
+	}
+	else
+	{
+		cout << "something went wrong with the text file" << endl;
+	}
+}
+
+void nfaParsing(NfaParser nfaparser)
+{
+	nfaparser.clearWhiteSpace(); // remove spaces and tabs
+	nfaparser.parse_nfa();
+}
+
+void nfaComputing(Menus menus, NfaParser nfaparser)
+{
+	string userInNFA = "";
+	while (userInNFA != "q")
+	{
+		menus.setMenu(Menus::NFA_OPS);
+		menus.printCurrentMenu();
+		cin >> userInNFA;
+		userInNFA = tolower(userInNFA[0]);
+
+		string w;
+		if (userInNFA == "w")
+		{
+			menus.setMenu(Menus::W_IN);
+			menus.printCurrentMenu();
+			NfaComputer computer;
+			cin >> w;
+			string result = computer.startExecution(w, nfaparser);
+			if (result == "true")
+			{
+				cout << "M accepts w" << endl;
+			}
+			else if (result == "false")
+			{
+				cout << "M does not accept w" << endl;
+			}
+			else
+			{
+				cout << "input string contained one or more characters not present in SIGMA" << endl;
+			}
+		}
+		else if (userInNFA == "q")
+		{
+			cout << "done with this nfa." << endl;
+		}
+		else
+		{
+			cout << "invalid input, try again" << endl;
+		}
+	}
+}
+
+void nfaHandling(Menus menus)
+{
+	string txt;
+	menus.setMenu(Menus::INPUT_NFA);
+	menus.printCurrentMenu();
+	cin >> txt;
+	// read nfa.txt
+	fstream newfile;
+	newfile.open(txt, ios::in);
+	NfaParser nfaparser;
+	if (newfile.is_open())
+	{
+		nfaparser.nfaDef = "";
+		// concatenate each line of dfa.txt to dfaDef, ignoring all newlines in text file
+		string line;
+		while (getline(newfile, line))
+		{
+			nfaparser.nfaDef += line;
+		}
+		nfaParsing(nfaparser);
+		nfaComputing(menus, nfaparser);
+	}
+	else
+	{
+		cout << "something went wrong with the text file" << endl;
+	}
+}
+
 int main()
 {
 	string userIn = "";
-	Menus m;
+	Menus menus;
 	while (userIn != "q")
 	{
-		m.setMenu(Menus::MAIN);
-		m.printCurrentMenu();
+		menus.setMenu(Menus::MAIN);
+		menus.printCurrentMenu();
 		cin >> userIn;
 		userIn = tolower(userIn[0]);
-		string txt;
+
 		if (userIn == "d")
 		{
-			m.setMenu(Menus::INPUT_DFA);
-			m.printCurrentMenu();
-			cin >> txt;
-			// read dfa.txt
-			fstream newfile;
-			newfile.open(txt, ios::in);
-			DfaParser dfaparser;
-			if (newfile.is_open())
-			{
-				dfaparser.dfaDef = "";
-				// concatenate each line of dfa.txt to dfaDef, ignoring all newlines in text file
-				string line;
-				while (getline(newfile, line))
-				{
-					dfaparser.dfaDef += line;
-				}
-				dfaparser.clearWhiteSpace(); // remove spaces and tabs
-
-				dfaparser.parse_dfa();
-				/*
-				 * parse dfa according to the CFG G (V, SIGMA, R, S)
-				 * where V = {dfa, q, sigma, delta, start_state, f, list, transitions, transition, primary},
-				 *		SIGMA = {Q, SIGMA, DELTA, START_STATE, F, EQUAL, LBRACE, RBRACE, SEMICOLON, LPAREN, RPAREN, ID, NUM},
-				 *		R = {
-				 *
-				 *		dfa			-> q sigma delta start_state f,
-				 *		q			-> Q EQUAL LBRACE list LBRACE SEMICOLON,
-				 *		sigma		-> SIGMA EQUAL LBRACE list RBRACE SEMICOLON,
-				 *		delta		-> DELTA EQUAL LBRACE transitions RBRACE SEMICOLON,
-				 *		start_state	-> START_STATE EQUAL primary SEMICOLON,
-				 *		f			-> F EQUAL LBRACE list RBRACE SEMICOLON,
-				 *		list		-> primary COMMA list || primary,
-				 *		transitions	-> transiton COMMA transitions || transition,
-				 *		transition	-> DELTA LPAREN primary COMMA primary RPAREN EQUALS primary,
-				 *		primary		-> ID || NUM}
-				 *
-				 *		S = dfa
-				 */
-				string userInDFA = "";
-
-				while (userInDFA != "q")
-				{
-					m.setMenu(Menus::DFA_OPS);
-					m.printCurrentMenu();
-					cin >> userInDFA;
-					userInDFA = tolower(userInDFA[0]);
-
-					string w;
-					if (userInDFA == "w")
-					{
-						m.setMenu(Menus::W_IN);
-						m.printCurrentMenu();
-						DfaComputer executor;
-						cin >> w;
-						if (executor.startExecution(w, dfaparser))
-							cout << "M accepts w" << endl;
-						else
-							cout << "M does not accept w" << endl;
-					}
-					else if (userInDFA == "q")
-					{
-						cout << "done with this dfa." << endl;
-					}
-					else
-					{
-						cout << "invalid input, try again" << endl;
-					}
-				}
-			}
-			else
-			{
-				cout << "something went wrong with the text file" << endl;
-			}
+			dfaHandling(menus);
 		}
 		else if (userIn == "n")
 		{
-			m.setMenu(Menus::INPUT_NFA);
-			m.printCurrentMenu();
-			cin >> txt;
-			// read nfa.txt
-			fstream newfile;
-			newfile.open(txt, ios::in);
-			NfaParser nfaparser;
-			if (newfile.is_open())
-			{
-				nfaparser.nfaDef = "";
-				// concatenate each line of dfa.txt to dfaDef, ignoring all newlines in text file
-				string line;
-				while (getline(newfile, line))
-				{
-					nfaparser.nfaDef += line;
-				}
-				nfaparser.clearWhiteSpace(); // remove spaces and tabs
-
-				nfaparser.parse_nfa();
-
-				string userInNFA = "";
-				while (userInNFA != "q")
-				{
-					m.setMenu(Menus::NFA_OPS);
-					m.printCurrentMenu();
-					cin >> userInNFA;
-					userInNFA = tolower(userInNFA[0]);
-
-					string w;
-					if (userInNFA == "w")
-					{
-						m.setMenu(Menus::W_IN);
-						m.printCurrentMenu();
-						NfaComputer executor;
-						cin >> w;
-						string result = executor.startExecution(w, nfaparser);
-						if (result == "true")
-						{
-							cout << "M accepts w" << endl;
-						}
-						else if (result == "false")
-						{
-							cout << "M does not accept w" << endl;
-						}
-						else
-						{
-							cout << "input string contained one or more characters not present in SIGMA" << endl;
-						}
-					}
-					else if (userInNFA == "q")
-					{
-						cout << "done with this nfa." << endl;
-					}
-					else
-					{
-						cout << "invalid input, try again" << endl;
-					}
-				}
-			}
-			else
-			{
-				cout << "something went wrong with the text file" << endl;
-			}
+			nfaHandling(menus);
 		}
 		else if (userIn == "q")
 		{
